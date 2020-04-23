e9acc40f5f3b1919742e55068a347bb69698e9a1
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-04-23 12:36:25 -0400

    adds connections to db production

--- knexfile.js
+++ knexfile.js
@@ -25,4 +25,17 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+
+
+  // Heroku will look for a production configuration
+  production: {
+    client: 'pg', // npm i pg
+    connection: process.env.DATABASE_URL, // produced by Heroku
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  }
 };
